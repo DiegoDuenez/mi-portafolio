e17a3c91a3a294cb390bd85caa631a2694d16d83
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-05-05 10:51:40 -0500

    upd: se modifico el scroll limit para quitar la clase de los links

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(window).scroll(function (event) {
+    var links = $('a.header__info-link')
     var scroll = $(window).scrollTop();
-    if(scroll < 15){
-        var links = $('a.header__info-link')
+    if(scroll <= 2){
         links.removeClass('header__info-link--active');
     }
 });
@@ -10,6 +10,7 @@ $(window).scroll(function (event) {
 $(function() {
     var links = $('a.header__info-link').click(function() {
         links.removeClass('header__info-link--active');
+        links.css('transition','400ms')
         $(this).addClass('header__info-link--active');
     });
 });
